e2711db9dc5f5170568a74ed814c49b84325dc1c
Radheshyam Singh <radheshyam.singh@pretlist.com>
2019-05-25 12:30:37 +0530

    Added setup for coroutine with live data demo

---
+++ coroutine-livedata/app/src/main/java/com/radheshyam/coroutinelivedatademo/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.radheshyam.coroutinelivedatademo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.radheshyam.coroutinelivedatademo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
